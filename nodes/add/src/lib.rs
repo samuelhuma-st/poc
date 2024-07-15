cb94aeec671c0b05d205cd6704597f3353a632de
samuelhuma-st <samuel.st@humaapi.com>
2024-07-15 19:41:49 +0200

    fix add nodes

--- nodes/add/src/lib.rs
+++ nodes/add/src/lib.rs
@@ -5,8 +5,22 @@ use exports::example::add::addnode::Guest;
 struct AddNode;
 
 impl Guest for AddNode {
-    fn execute() -> String {
-        String::from("user")
+    fn execute(params: String) -> String {
+        let mut sum = 0;
+        if let Ok(value) = serde_json::from_str::<Vec<i32>>(params.as_str()) {
+            // Calculer la somme des nombres dans le vecteur
+            sum = value.iter().sum();
+        } else {
+            wasi::logging::logging::log(
+                wasi::logging::logging::Level::Info,
+                "",
+                &format!("Error while converting value in AddNode"),
+            );
+            // Retourner une erreur ou une valeur par défaut si le paramètre n'est pas trouvé
+            return "Numbers to add not found".to_string();
+        }
+
+        format!("{sum}").to_string()
     }
 }
 
